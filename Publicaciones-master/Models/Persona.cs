ee457854f95ed476c55e20bc7dd029ccc096ebfa
Bryan <bmirandag@hotmail.com>
2016-12-16 18:40:07 -0800

    'ProyectoSoft'

---
+++ Publicaciones-master/Models/Persona.cs
@@ -0,0 +1,23 @@
+namespace Publicaciones.Models
+{
+    public class Persona
+    {
+        public string Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Email{get; set; }
+
+        public string FechaNacimiento{get; set;} 
+        
+        public string Nacionalidad{get; set;}
+        
+        public string Genero{get; set;}
+
+        public List<Autor> Autor{get; set;}
+
+    }
+
+}
