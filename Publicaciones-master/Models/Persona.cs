2348eb059e7c604500ad6c67271765f148f2a9b3
Bryan <bmirandag@hotmail.com>
2016-12-18 14:44:01 -0800

    Implementacion de clases: Autor, Persona, Publicacion

--- Publicaciones-master/Models/Persona.cs
+++ Publicaciones-master/Models/Persona.cs
@@ -1,12 +1,56 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 namespace Publicaciones.Models
 {
+    /// <summary>
+    /// Clase encargada de representar una persona.
+    /// </summary>
+    /// <returns></returns>
     public class Persona
     {
-        public string Id { get; set; }
+        [Key]
+        /// <summary>
+        /// Rut de la persona.
+        /// </summary>
+        /// <returns></returns>
+        public string Rut { get; set; }
 
+        /// <summary>
+        /// Nombre de la persona.
+        /// </summary>
+        /// <returns></returns>
         public string Nombre { get; set; }
 
+        /// <summary>
+        /// Apellido de la persona.
+        /// </summary>
+        /// <returns></returns>
         public string Apellido { get; set; }
+
+        /// <summary>
+        /// Email de la persona.
+        /// </summary>
+        /// <returns></returns>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Fecha de nacimiento de la persona.
+        /// </summary>
+        /// <returns></returns>        
+        public int FechaNacimiento { get; set; }
+
+        /// <summary>
+        /// Nacionalidad de la persona.
+        /// </summary>
+        /// <returns></returns>
+        public string Nacionalidad { get; set; }
+
+        /// <summary>
+        /// Genero de la persona.
+        /// </summary>
+        /// <returns></returns>
+        public string Genero { get; set; }
     
     }
 }
---
+++ Publicaciones-master/Models/Publicacion.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace Publicaciones.Models
+{
+    /// <summary>
+    /// Clase encargada de representar una publicacion.
+    /// </summary>
+    /// <returns></returns>
+    public class Publicacion
+    {
+        [Key]
+        /// <summary>
+        /// Identificador de la publicacion.
+        /// </summary>
+        /// <returns></returns>
+        public string Doi { get; set; }
+
+        /// <summary>
+        /// Titulo de la publicacion.
+        /// </summary>
+        /// <returns></returns>
+        public string Titulo { get; set; }
+
+        /// <summary>
+        /// Volumen de la publicacion.
+        /// </summary>
+        /// <returns></returns>
+        public string Volumen { get; set; }
+
+        /// <summary>
+        /// Pagina de inicio de la publicacion.
+        /// </summary>
+        /// <returns></returns>
+        public string PaginaInicio  { get; set; }
+
+        /// <summary>
+        /// Pagina final de la publicacion.
+        /// </summary>
+        /// <returns></returns>
+        public string PaginaFinal { get; set; }
+
+        /// <summary>
+        /// Cantidad de Rechazos que ha tenido la publicacion.
+        /// </summary>
+        /// <returns></returns>
+        public string ContidadRechazos { get; set; }
+
+        /// <summary>
+        /// Numero de paginas que tiene la revista.
+        /// </summary>
+        /// <returns></returns>
+        public string NumeroDePagina{get; set;}
+    }
+
+}
