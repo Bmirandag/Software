d7cb8f27ab866beed84b56b34eac483026a4dc9a
rorro1805 <racdrigo123@hotmail.com>
2016-12-18 23:00:04 -0300

    'Actualizacion'

---
+++ Publicaciones-master/Models/Grado.cs
@@ -0,0 +1,19 @@
+namespace Publicaciones.Models
+{
+    
+    public class Grado
+    {
+        /// <summary>
+        /// Nombre del grado academico
+        /// </summary>
+        /// <returns></returns>
+        private string nombre { get; set;}
+
+        /// <summary>
+        /// Fecha en que se obtuvo el grado academico
+        /// </summary>
+        /// <returns></returns>
+        private string fecha { get; set;}
+    }   
+
+}
