eb8822b40023905bd282a6441d8aaef48127487b
DavidMezaAstengo <sda>
2016-12-21 08:03:04 -0300

    Added: ObtenerPublicacionesTest()

--- Publicaciones-master/Test/MainServiceTests.cs
+++ Publicaciones-master/Test/MainServiceTests.cs
@@ -72,6 +72,34 @@ namespace Publicaciones.Service {
         }
 
         [Fact]
+        public void ObtenerPublicacionesTest(){
+
+        Logger.LogInformation("Testing IMainService.Publicaciones() ..");
+         List<Publicacion> publicaciones = Service.Publicaciones();
+            // Debe ser !=  de null
+            Assert.True(publicaciones != null);
+
+            // Debe retornar vacia
+            Assert.True(publicaciones.Count == 0);
+        
+         for (int i = 0; i < 5; i++) {
+            Publicacion publicacion = new Publicacion(); 
+            publicacion.Doi = ""+i; 
+            publicacion.Titulo = "Titulo "+i; 
+            publicacion.PaginaInicio = "1"; 
+            publicacion.PaginaFinal = "1"; 
+            publicacion.CantidadRechazos = "0"; 
+            publicacion.NumeroDePagina = "1";
+            Service.AddPublicacion(publicacion);
+            }
+        // Debe retornar con 5
+         Assert.True(publicaciones.Count == 5);
+
+        }
+
+
+
+        [Fact]
         public void FindPersonasTest(){
             Logger.LogInformation("Testing IMainService.FindPersonas(string rut) ..");
             Service.Initialize();
